c8ba33242626829da8a70db6de30ced5a16a2ea4
Chris Rhee <0407chan@naver.com>
2024-10-03 14:31:14 +0900

    feat: color secondary추가

--- lib/constants/color.dart
+++ lib/constants/color.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const primary = Color.fromARGB(255, 234, 78, 73);
-  static const secondary = Colors.redAccent;
+  static const secondary = Color.fromARGB(105, 121, 40, 37);
 
   static const white = Colors.white;
   static const whiteSecondary = Colors.white60;
