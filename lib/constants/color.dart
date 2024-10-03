6fea5b363f7bc0df94a0a829d5547745bb481ed8
Chris Rhee <0407chan@naver.com>
2024-10-03 14:07:24 +0900

    feat: color constants 생성

---
+++ lib/constants/color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color.fromARGB(255, 234, 78, 73);
+  static const secondary = Colors.redAccent;
+
+  static const white = Colors.white;
+  static const whiteSecondary = Colors.white60;
+}
